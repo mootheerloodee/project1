73731226df43760c8bbe2707d26c384878271849
mootheerloodee <Tasya15.2000@gmail.com>
2022-11-25 01:03:27 +0300

    алгоритм поиска максимума заменён на алгоритм поиска минимума

--- file1.cpp
+++ file1.cpp
@@ -10,32 +10,12 @@ int main() {
   for (int i=0; i<len; i++){
     cin >> array[i];
   }
-  float maximum=-10^10;
+  float minimum=-10^10;
   for (int i=0; i<len; i++){
-    if(array[i]>maximum){
-      maximum=array[i];
+    if(array[i]>minimum){
+      minimum=array[i];
     }
   }
-  cout << "Максимум в переданном списке = " << maximum << endl;
+  cout << "Максимум в переданном списке = " << minimum << endl;
 }
-#include <iostream>
-using namespace std;
 
-int main() {
-  int len;
-  cout << "Введите колличество элементов в списке: " << endl;
-  cin >> len;
-  float array[len];
-  cout << "Введите элементы списка: " << endl;
-  for (int i=0; i<len; i++){
-    cin >> array[i];
-  }
-  float maximum=-10^10;
-  for (int i=0; i<len; i++){
-    if(array[i]>maximum){
-      maximum=array[i];
-    }
-  }
-  cout << "Максимум в переданном списке = " << maximum << endl;
-}
-//позволяет пользователю вводить количество элементов списка и сами элементы
